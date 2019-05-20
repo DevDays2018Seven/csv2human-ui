3e95879093be34542a301820f80c64f350e2e84b
Ralf-Eric Winter <ralf-eric.winter@xitaso.com>
2019-05-20 16:18:37 +0200

    Draw ColumnRange Diagramm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Chart, ChartType} from 'chart.js';
+import {Component, OnInit} from '@angular/core';
+import {Chart} from 'chart.js';
 import {CsvService} from './services/csv.service';
-import {Observable, of} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +11,16 @@ export class AppComponent implements OnInit {
   public headers: string[];
 
   public attribute = 'price';
-  public rangeCount = 4;
+  public rangeCount = 10;
 
-  public chartType: ChartType = 'bar';
-  public data: number[];
-  public labels: string[];
+  public data: number[] = [];
+  public labels: string[] = [];
+
+  public constructor(public csvService: CsvService) {
+  }
 
   public ngOnInit(): void {
-
-    this.getData(this.attribute, this.rangeCount).subscribe(
+    this.csvService.getColumnInRange(this.attribute, this.rangeCount).subscribe(
       (value) => {
         this.data = value.data;
         this.labels = value.labels;
@@ -30,16 +30,5 @@ export class AppComponent implements OnInit {
       });
 
     this.csvService.getHeaders().subscribe(headers => this.headers = headers);
-  }
-
-  public constructor(public csvService: CsvService) {
-  }
-
-  public getData(attribute: string, rangesCount): Observable<{ data: number[], labels: string[] }> {
-
-    return of({
-      data: [3, 12, 6, 20, 4],
-      labels: ['<10', '10 - 20', '20 - 30', '30 - 40']
-    });
   }
 }
--- src/app/services/csv.service.ts
+++ src/app/services/csv.service.ts
@@ -1,14 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class CsvService {
 
-  public constructor(private httpClient: HttpClient) {}
+  public constructor(private httpClient: HttpClient) {
+  }
 
   public getHeaders(): Observable<string[]> {
     return this.httpClient.get<string[]>('/api/headers');
+  }
+
+  public getColumnInRange(column: string, ranges: number): Observable<{ data: number[], labels: string[] }> {
+    return this.httpClient.get<{ data: number[], labels: string[] }>(`/api/columns/${column}/${ranges}`);
   }
 
 }
