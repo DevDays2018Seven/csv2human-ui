fa169b4a2f85502268c194b24f89193c6d8180c1
Andreas Pfohl <mail@apfohl.com>
2019-05-20 19:00:14 +0200

    Prepared selection of csv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {Chart} from 'chart.js';
-import {CsvService} from './services/csv.service';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { CsvService } from './services/csv.service';
 
 @Component({
   selector: 'app-root',
@@ -11,20 +11,30 @@ export class AppComponent implements OnInit {
   public headers: string[];
 
   public attribute = 'review.point';
+
   public rangeCount = 10;
 
   public data: number[] = [];
+
   public labels: string[] = [];
 
   public ranges = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
+  public csvs: string[];
+
   public selectedHeader = this.attribute;
+
   public selectedRangeWidth = this.rangeCount;
+
+  public selectedCsv;
+
+  public outlier = false;
 
   public constructor(public csvService: CsvService) {
   }
 
   public ngOnInit(): void {
+    // this.fetchCsvs();
     this.fetchData(this.attribute, this.rangeCount);
 
     this.csvService.getHeaders().subscribe(headers => this.headers = headers);
@@ -34,14 +44,19 @@ export class AppComponent implements OnInit {
     this.fetchData(this.selectedHeader, this.selectedRangeWidth);
   }
 
+  private fetchCsvs(): void {
+    this.csvService.fetchCsvs().subscribe(
+      value => {
+        this.csvs = value;
+        this.selectedCsv = value[0];
+      }, console.error);
+  }
+
   private fetchData(attribute: string, rangeCount: number): void {
-    this.csvService.getColumnInRange(attribute, rangeCount).subscribe(
-      (value) => {
+    this.csvService.getColumnInRange(attribute, rangeCount, this.outlier).subscribe(
+      value => {
         this.data = value.data;
         this.labels = value.labels;
-      },
-      (error) => {
-        console.log(error);
-      });
+      }, console.error);
   }
 }
--- src/app/services/csv.service.ts
+++ src/app/services/csv.service.ts
@@ -10,12 +10,20 @@ export class CsvService {
   public constructor(private httpClient: HttpClient) {
   }
 
+  public fetchCsvs(): Observable<string[]> {
+    return this.httpClient.get<string[]>(`${this.basePath}/csv`);
+  }
+
+  public setCsv(name: string): Observable<void> {
+    return this.httpClient.post<void>(`${this.basePath}/csv/${name}`, null);
+  }
+
   public getHeaders(): Observable<string[]> {
     return this.httpClient.get<string[]>(`${this.basePath}/headers`);
   }
 
-  public getColumnInRange(column: string, ranges: number): Observable<{ data: number[], labels: string[] }> {
-    return this.httpClient.get<{ data: number[], labels: string[] }>(`${this.basePath}/plot/distribution/${column}/${ranges}`);
+  public getColumnInRange(column: string, ranges: number, outlier: boolean = false): Observable<{ data: number[], labels: string[] }> {
+    return this.httpClient.get<{ data: number[], labels: string[] }>(`${this.basePath}/plot/distribution/${column}/${ranges}${outlier ? '?outlier=dixon-q' : ''}`);
   }
 
 }
