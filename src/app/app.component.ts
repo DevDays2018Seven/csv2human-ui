dae3b7a2f403036be26ed4625cd1694b5bae1749
Andreas Pfohl <mail@apfohl.com>
2019-05-20 13:14:06 +0200

    Added CsvService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
+import { CsvService } from './services/csv.service';
 
 @Component({
   selector: 'app-root',
@@ -14,18 +15,50 @@ export class AppComponent implements OnInit {
 
   public context: CanvasRenderingContext2D;
 
+  public headers: string[];
+
   public ngOnInit(): void {
     this.context = (this.canvas.nativeElement as HTMLCanvasElement).getContext('2d');
 
     this.chart = new Chart(this.context, {
-      type: 'line',
+      type: 'bar',
       data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
           label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3]
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
         }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
       }
     });
+
+    this.csvService.getHeaders().subscribe(headers => this.headers = headers);
   }
+
+  public constructor(public csvService: CsvService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CsvService } from './services/csv.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    CsvService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
